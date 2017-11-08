d2259958851320164bf7fb6b164866289b52f4e7
harryemery <33031357+harryemery@users.noreply.github.com>
2017-11-08 23:30:17 +0000

    Added register functionality to the servlet
    Is not tested as the JSP requires changes first

--- XYZDrivers/src/java/com/xyzdrivers/Register.java
+++ XYZDrivers/src/java/com/xyzdrivers/Register.java
@@ -7,6 +7,15 @@ package com.xyzdrivers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,11 +37,86 @@ public class Register extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         RequestDispatcher view = request.getRequestDispatcher("register.jsp");
         view.forward(request, response);
+        String[] info = new String[2];
+        info[0] = request.getParameter("name");
+        info[1] = request.getParameter("address");
+        info[2] = request.getParameter("dob");
+        
+        try {
+            registerMember(info);
+        } catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void registerMember(String[] info) throws SQLException{
+        String host = "jdbc:derby//localhost:1527/ESD";
+        String user = "root";
+        
+        Connection con = DriverManager.getConnection(host, user, null);
+         
+        String name = info[0];
+        String address = info[1];
+        String dob = info[2];
+        LocalDate registerDate = LocalDate.now();
+        String status = "APPLIED";
+        double balance = 0;
+        
+        String id = generateID(name);
+
+        String members = "INSERT INTO 'MEMBERS' ('id','name','address','dob','dor','status','balance')"
+                + "VALUES ('" + id + "','" + name + "','" + address + "','" + dob + "','" + registerDate + "','" + status + "','" + balance + "')";
+
+        String users = "INSERT INTO 'USERS' ('id','password','status'" + 
+                "VALUES ('" + id + "','" + generatePassword() + "','" + status + "')";
+        
+        Statement state;
+        try {
+            state = con.createStatement();
+            state.executeQuery(members);
+            state.executeQuery(users);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }
+    
+    public String generateID(String name){
+        StringBuilder id = new StringBuilder();
+        
+        id.append(name.charAt(0)).append("-");
+        
+        int space = name.indexOf(' ');
+        
+        for(int i = space + 1; i < name.length(); i++){
+            id.append(name.charAt(i));
+        }
+        
+        return id.toString();
+    }
+    
+    
+    public String generatePassword(){
+        String chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+        StringBuilder password = new StringBuilder();
+        Random ran = new Random();
+        for(int i = 0; i < 6; i++){
+            password.append(chars.charAt(ran.nextInt(6)));
+        }
+        
+        
+        return password.toString();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- XYZDrivers/web/register.jsp
+++ XYZDrivers/web/register.jsp
@@ -13,37 +13,50 @@
     </head>
     <body>
         <h1>Oh look a register page!</h1>
-<<<<<<< HEAD
-=======
+        <<<<<<< HEAD
+        =======
+
+
+
+
+
+
+
+
+        <a id="Navigate" href="./index.html">
+            <input 
+
+                type="button"
+                id="homeButton"
+                style="
+
+                background-image: url(http://cdn3.blogsdna.com/wp-content/uploads/2010/03/Windows-Phone-7-Series-Icons-Pack.png);
+                background-repeat: no-repeat;
+                background-position: -272px -112px;
+                cursor:pointer;
+                height: 40px;
+                width: 40px;
+                border-radius: 26px;
+                border-style: solid;
+                border-color:#000;
+                border-width: 3px;" title="Navigate"
+                />
+        </a>  
+        <p>
+          <label>Name:</label>
+          <input type = "text"
+                 id = "nameText"
+                 value = "" />
+        </p>
         
+        <p>
+            <input type ="button"
+                   id ="registerButton"
+                   value ="Register"
         
-        
-        
-        
-        
-        
-        
-          <a id="Navigate" href="./index.html">
-  <input 
-    
-    type="button"
-    id="homeButton"
-    style="
-	
-      background-image: url(http://cdn3.blogsdna.com/wp-content/uploads/2010/03/Windows-Phone-7-Series-Icons-Pack.png);
-      background-repeat: no-repeat;
-      background-position: -272px -112px;
-      cursor:pointer;
-      height: 40px;
-      width: 40px;
-      border-radius: 26px;
-      border-style: solid;
-      border-color:#000;
-      border-width: 3px;" title="Navigate"
-    />
-</a>  
-        
-    <p>Today's date: <%= (new java.util.Date()).toLocaleString()%></p>    
->>>>>>> david
+        </p>
+
+        <p>Today's date: <%= (new java.util.Date()).toLocaleString()%></p>    
+        >>>>>>> david
     </body>
 </html>
